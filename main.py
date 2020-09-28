333c9fd31b80d4ea852746e73ec4d463947338ca
Adrian <adrian_zinko@op.pl>
2020-09-28 15:52:30 +0200

    feat: setup connection with smtp mail server

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ from os import getenv
 import jinja2
 
 load_dotenv()
+
+class GmailAdapter:
+  def __init__(
+    selt, 
+    host:str, 
+    port:int, 
+    username:str, 
+    password:str
+  ):
+  self.host = host
+  self.port = port
+  self.username = username
+  self.password = password
+  self.server = smtplib.SMTP_SSL(host, post)
+
+  def login(self): 
+    self.server.ehlo()
+    self.server.login(self.username, self.password)
+  
+  def __del__(self):
+    self.server.close()
